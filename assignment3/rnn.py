26e276eb23069076d394a33313b60b995e2f586c
Dmitri Nesterenko <dmitri@sentimentalistmag.com>
2017-09-16 02:28:42 +0000

    Use embed size of 1400

--- assignment3/rnn.py
+++ assignment3/rnn.py
@@ -18,7 +18,7 @@ class Config(object):
     """Holds model hyperparams and data information.
        Model objects are passed a Config() object at instantiation.
     """
-    embed_size = 700
+    embed_size = 1400
     label_size = 2
     early_stopping = 2
     anneal_threshold = 1.0
